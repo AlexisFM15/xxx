8b7e2fcaa54fd381094f2267647c280497b24391
AlexisFM15 <maximoalexisflorianmatos@gmail.com>
2022-09-06 14:16:17 -0400

    Segundo Ejercicio de javascript

---
+++ Segundo_ejercicio_de_Javascript.js
@@ -0,0 +1,22 @@
+
+
+const Nombre= "Alexis";
+const edad = 21;
+const desarrollador = true;
+const fechaNac = new Date(2001,7,15);
+
+const librofavorito ={
+    titulo: "La ultima Nota",
+    autor: "Joana Marcus",
+    año: new Date(2006),
+    url: "https://www.wattpad.com/738548402-la-%C3%BAltima-nota-sinopsis"
+}
+const Lista = [
+    Nombre, 
+    edad, 
+    desarrollador, 
+    fechaNac, 
+    librofavorito
+]
+
+console.log(Lista)
